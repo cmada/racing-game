034f36b10a0d8d78012f641979fdf6cdf19e6bb2
cmada <madalina.ciceu@gmail.com>
2020-10-04 16:46:33 +0300

    Initial commit (project structure)

---
+++ src/main/java/org/cmada/App.java
@@ -0,0 +1,14 @@
+package org.cmada;
+
+/**
+ * Hello world!
+ *
+ */
+// one-line comment
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
