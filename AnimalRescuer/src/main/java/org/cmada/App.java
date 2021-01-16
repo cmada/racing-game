56d63d2f04b3ef4c574b22553718e01657617093
cmada <madalina.ciceu@gmail.com>
2021-01-16 21:12:26 +0200

    supraincarcare metode si constructori

---
+++ AnimalRescuer/src/main/java/org/cmada/App.java
@@ -0,0 +1,55 @@
+package org.cmada;
+
+import java.time.LocalDateTime;
+
+public class App
+{
+    public static void main(String[] args) {
+        Rescuer rescuer = new Rescuer();
+        rescuer.name = "John";
+        rescuer.setMoney(300);
+
+        Animal animal = new Animal();
+
+        animal.setName("Thor");
+        animal.setAge(3.5);
+        animal.setHealthLevel(10);
+        animal.setHungerLevel(5);
+        animal.setFavoriteActivityName("Eating");
+        animal.setFavoriteFoodName("Ceafa");
+
+
+        EntertainmentActivity activity = new EntertainmentActivity("Eating") {
+            @Override
+            public String getName() {
+                return null;
+            }
+        };
+
+        Food food = new Food("Ceafa");
+        food.setQuantity(9);
+        food.setPrice(55.55);
+        food.setInStock(false);
+        food.setExpiryDate(LocalDateTime.of(2018, 12, 31, 12,0,0));
+
+        MedicalStaff medic = new MedicalStaff();
+        medic.name = "Dr.Vet";
+        medic.setSpecialization("Veterinary Medicine");
+
+        Game game = new Game();
+        game.setRescuer(rescuer);
+        game.setAnimal(animal);
+        game.setMedic(medic);
+
+        rescuer.feed(animal, food);
+        rescuer.entertain(animal,activity);
+
+
+
+
+
+        
+
+
+    }
+}
